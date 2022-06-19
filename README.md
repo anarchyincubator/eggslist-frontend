684ff18320dbc46f70e6becff5ba309062af268a
Ivan <i.kudrya@sports.ru>
2022-06-20 02:26:46 +0300

    1: Init commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# eggslist-frontend
+# Nuxt 3 Awesome Starter
+
+## Quick Start
+* This project using `yarn` as package manager.
+* Clone this project to your computer `git clone https://github.com/viandwi24/nuxt3-awesome-starter`
+* Install dependencies `yarn install`
+* Run `yarn dev` to start development server and open `http://localhost:3000` in your browser
+* Run `yarn build` to build project and `yarn start` to start production server
---
+++ components/Page/Navbar.vue
@@ -0,0 +1,139 @@
+<script lang="ts" setup>
+import { computed } from '@vue/reactivity'
+import { IApp } from '@/utils/app'
+
+export interface IMenuItem {
+  type: 'link' | 'button'
+  text: string
+  href?: any
+  route?: any
+}
+
+const app = useState<IApp>('app')
+const menus = computed((): IMenuItem[] => [
+  { type: 'link', text: 'blan', route: { name: 'blank' } },
+  { type: 'link', text: 'tes', route: { name: 'test' } },
+  { type: 'link', text: 'abou', route: { name: 'about' } },
+  { type: 'link', text: 'settin', route: { name: 'setting' } },
+  {
+    type: 'button',
+    text: 'nav',
+    route: { name: 'dashboard' },
+  },
+])
+</script>
+
+<template>
+  <div></div>
+  <!-- <BuilderNavbar>
+    <template #banner>
+      <div
+        class="text-white text-xs text-center py-1 px-4 lg:px-8 bg-primary-500 capitalize"
+      >
+        <span class="mr-1"> 'egg' </span>
+        <Anchor
+          class="underline font-bold"
+          href="https://github.com/viandwi24/nuxt3-awesome-starter"
+        />
+      </div>
+    </template>
+    <template #menu>
+      <div class="relative hidden lg:flex items-center ml-auto">
+        <nav
+          class="text-sm leading-6 font-semibold text-gray-600 dark:text-gray-300"
+          role="navigation"
+        >
+          <ul class="flex items-center space-x-8">
+            <li v-for="(item, i) in menus" :key="i">
+              <Anchor
+                v-if="item.type === 'link'"
+                :to="item.route ? item.route : undefined"
+                :href="item.href ? item.href : undefined"
+                class="hover:no-underline hover:text-slate-900 hover:dark:text-white capitalize"
+                >{{ item.text }}</Anchor
+              >
+              <Button
+                v-else-if="item.type === 'button'"
+                :text="item.text"
+                size="xs"
+                class="font-extrabold capitalize"
+                :to="item.route ? item.route : undefined"
+                :href="item.href ? item.href : undefined"
+              />
+            </li>
+          </ul>
+        </nav>
+        <div
+          class="flex space-x-4 border-l ml-6 pl-6 border-gray-900/10 dark:border-gray-50/[0.2]"
+        >
+          <LanguageSwitcher />
+          <ThemeSwitcher />
+          <Anchor
+            class="hover:no-underline hover:text-slate-900 hover:dark:text-white text-lg flex self-center items-center"
+            href="https://github.com/viandwi24/nuxt3-awesome-starter"
+            title="Github"
+          >
+            <IconMdi:github-face />
+          </Anchor>
+        </div>
+      </div>
+    </template>
+    <template #options="{ toggleOptions }">
+      <ActionSheet @onClose="toggleOptions(false)">
+        <ActionSheetBody>
+          <ActionSheetHeader text="Menu" />
+          <nav class="leading-6 font-semibold text-gray-600 dark:text-gray-300">
+            <ul class="flex flex-col">
+              <li
+                v-for="(item, i) in menus"
+                :key="i"
+                class="flex w-full"
+                :class="{
+                  'pb-2 mb-2 border-b border-gray-900/10 dark:border-gray-50/[0.2]':
+                    item.type === 'link',
+                }"
+              >
+                <Anchor
+                  v-if="item.type === 'link'"
+                  :to="item.route ? item.route : undefined"
+                  :href="item.href ? item.href : undefined"
+                  class="flex-1 hover:no-underline capitalize"
+                  >{{ item.text }}</Anchor
+                >
+                <Button
+                  v-else-if="item.type === 'button'"
+                  :text="item.text"
+                  size="xs"
+                  class="flex-1 font-extrabold capitalize"
+                  :to="item.route ? item.route : undefined"
+                  :href="item.href ? item.href : undefined"
+                />
+              </li>
+            </ul>
+          </nav>
+          <div class="mt-6 text-sm font-bold capitalize">change</div>
+          <div class="mt-2">
+            <ThemeSwitcher type="select-box" />
+          </div>
+          <div class="mt-6 text-sm font-bold capitalize">hange lang</div>
+          <div class="mt-2">
+            <LanguageSwitcher type="select-box" />
+          </div>
+        </ActionSheetBody>
+        <Button
+          type="secondary"
+          title="Github"
+          href="https://github.com/viandwi24/nuxt3-awesome-starter"
+        >
+          <IconMdi:github-face />
+          <span class="ml-1">Github</span>
+        </Button>
+        <Button
+          text="Close"
+          type="secondary"
+          @click.prevent="toggleOptions(false)"
+        />
+      </ActionSheet>
+    </template>
+  </BuilderNavbar> -->
+</template>
