12165f6b72cd3d8662f5d10e8ec8462054ff9f26
Ivan <i.kudrya@sports.ru>
2022-07-10 00:23:59 +0300

    eggslist-7: add assets

--- components/Common/CustomButton.vue
+++ components/Common/CustomButton.vue
@@ -1,13 +1,165 @@
 <template>
-$END$
+  <component
+    :is="type"
+    :class="classes"
+    :href="href"
+    rel="noopener"
+    target="_blank"
+    :to="to"
+    @click="handleClick"
+  >
+    <span class="button__text">
+      <span v-if="isLoading" class="button__loader" />
+      <slot v-else />
+    </span>
+  </component>
 </template>
 
 <script>
 export default {
-name: "CustomButton.vue"
-}
+  name: "CustomButton",
+  props: {
+    href: {
+      type: String,
+      default: null,
+    },
+    to: {
+      type: String,
+      default: null,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    theme: {
+      type: String,
+      required: true,
+      validator(value) {
+        return ["primary", "secondary", "outlined"].includes(value);
+      },
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["click"],
+  computed: {
+    type() {
+      if (this.href) return "a";
+
+      if (this.to) return "router-link";
+
+      return "button";
+    },
+    classes() {
+      return [
+        "button",
+        `button-${this.theme}`,
+        {
+          "button-disabled": this.isDisabled || this.isLoading,
+        },
+      ];
+    },
+  },
+  methods: {
+    handleClick(event) {
+      if (this.isDisabled) return;
+
+      this.$emit("click", event);
+    },
+  },
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.button {
+  cursor: pointer;
+  background: none;
+  border: none;
+  padding: 0;
+  transition: background 0.75s;
 
+  &__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__loader {
+    display: inline-block;
+    width: vw(14px);
+    height: vw(14px);
+
+    @include layout-mobile() {
+      width: mvw(14px);
+      height: mvw(14px);
+    }
+  }
+
+  &__loader:after {
+    content: " ";
+    display: block;
+    width: vw(18px);
+    height: vw(18px);
+    border-radius: 50%;
+    border: vw(3px) solid #fff;
+    border-color: #fff transparent #fff transparent;
+    animation: loader-animation 1.2s linear infinite;
+
+    @include layout-mobile() {
+      width: mvw(18px);
+      height: mvw(18px);
+      border-width: mvw(3px);
+    }
+  }
+
+  &-primary,
+  &-secondary,
+  &-outlined {
+    display: flex;
+    justify-content: center;
+    padding: vw(10px) vw(20px);
+    background: $primary-marigold;
+    border: 1.5px solid #282220;
+    border-radius: vw(12px);
+    text-align: center;
+
+    @include layout-mobile() {
+      width: 100%;
+      border-radius: mvw(12px);
+      padding: mvw(16px) mvw(90px);
+    }
+
+    &:hover {
+      background: $secondary-marigold;
+    }
+
+    .button__text {
+      text-align: center;
+      color: $primary-black;
+    }
+  }
+  &-secondary {
+    background: transparent;
+    border: 1.5px solid $primary-marigold;
+  }
+  &-outlined {
+    background: transparent;
+    border: max(1px, vw(1px)) solid #fef6f4;
+  }
+  &-disabled {
+    pointer-events: none;
+    cursor: not-allowed;
+    background: #201e1d;
+  }
+}
+@keyframes loader-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
---
+++ components/Page/MobileNavbar.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="navbar-container" :class="{ 'navbar-container--opened': opened }">
+    <div class="navbar-container__top">
+      <img src="@/assets/images/icons/logo_dark.svg" />
+      <Hamburger v-model="opened" />
+    </div>
+    <div v-if="opened" class="navbar-container-opened">
+      <nuxt-link class="ceil menu-1" to="/market"> Market </nuxt-link>
+      <nuxt-link class="ceil menu-1" to="/about"> About </nuxt-link>
+      <nuxt-link class="ceil menu-1" to="/stories"> Our stories </nuxt-link>
+      <div
+        v-if="!isAuthenticated"
+        class="ceil menu-1"
+        @click="handleClickLogin"
+      >
+        Login
+      </div>
+      <nuxt-link v-else class="ceil menu-1" to="">Favorite Farmers</nuxt-link>
+      <div v-if="isAuthenticated" class="navbar-container__line" />
+      <div v-if="isAuthenticated" class="navbar-container__avatar ceil">
+        <img src="@/assets/images/avatar_mock.png" alt="" />
+        <span class="menu-1"> Anna</span>
+      </div>
+      <custom-button theme="primary" class="navbar-container__button button-2">
+        Post A Listing
+      </custom-button>
+      <custom-button
+        v-if="isAuthenticated"
+        theme="secondary"
+        class="navbar-container__button--bottom button-2"
+      >
+        Write a Story
+      </custom-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import CustomButton from "../common/CustomButton";
+import Hamburger from "../common/Hamburger";
+export default {
+  name: "MobileNavbar",
+  components: {
+    Hamburger,
+    CustomButton,
+  },
+  data() {
+    return {
+      opened: false,
+    };
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters["auth/isAuthenticated"];
+    },
+  },
+  methods: {
+    handleClickLogin() {
+      this.$store.dispatch("auth/setToken", "3213131");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$height-top: mvw(60px);
+$padding-bottom: mvw(32px);
+.ceil {
+  width: 100%;
+  height: mvw(56px);
+  display: flex;
+  align-items: center;
+}
+.navbar-container {
+  position: fixed;
+  background: $primary-white;
+  margin-left: -$padding-left-mobile;
+  padding: 0 $padding-left-mobile;
+  width: calc(100% - #{$padding-left-mobile} * 2);
+  &--opened {
+    height: 100%;
+  }
+  &__top {
+    width: 100%;
+    display: flex;
+    padding: mvw(18px) 0;
+    align-items: center;
+    img {
+      width: mvw(102px);
+      margin-right: auto;
+    }
+  }
+  &-opened {
+    display: flex;
+    height: calc(100% - #{$height-top} - #{$padding-bottom});
+    flex-direction: column;
+  }
+  &__no-auth {
+    display: flex;
+    flex-direction: column;
+  }
+  &__button {
+    margin-top: auto;
+    &--bottom {
+      margin-top: mvw(10px);
+    }
+  }
+  &__line {
+    border: 1px solid #d5bb91;
+    margin: mvw(8px) 0;
+  }
+  &__avatar {
+    display: flex;
+    img {
+      width: mvw(24px);
+      height: mvw(24px);
+      margin-right: mvw(8px);
+    }
+  }
+}
+</style>
