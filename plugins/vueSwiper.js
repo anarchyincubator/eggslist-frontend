e5cea04e742a74fbfe1873163ff3bee3b32944aa
Ivan <i.kudrya@sports.ru>
2022-07-14 22:50:29 +0300

    legacy update

--- plugins/vueSwiper.js
+++ plugins/vueSwiper.js
@@ -3,6 +3,6 @@ import VueAwesomeSwiper from "vue-awesome-swiper";
 
 // import style
 // If you use Swiper 6.0.0 or higher
-import "swiper/swiper-bundle.css";
+import "swiper/css/swiper.css";
 
 Vue.use(VueAwesomeSwiper /* { default options with global component } */);
