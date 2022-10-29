5973bbb7844949c4c1018b8468e99e54a16fbd3e
Ivan <i.kudrya@sports.ru>
2022-10-29 18:33:45 +0300

    add terms and privacy

---
+++ pages/terms.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page">
+    <div v-html="inner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TermsPage",
+  data() {
+    return {
+      inner: null,
+    };
+  },
+  async mounted() {
+    await this.$axios
+      .get(`${window.location.origin}/user_terms.html`)
+      .then((h) => {
+        this.inner = h.data;
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+  padding-top: 7rem;
+  padding-bottom: 5rem;
+}
+</style>
